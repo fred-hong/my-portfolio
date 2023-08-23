01c97518051493062b45cfdd5367b73b90d37f98
hong-fred <97639915+f-hong9@users.noreply.github.com>
2023-08-23 00:10:38 -0400

    Edit

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -8,10 +8,7 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <HashRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </HashRouter>
+  // <HashRouter>
+  <App />
+  // </HashRouter>
 );
